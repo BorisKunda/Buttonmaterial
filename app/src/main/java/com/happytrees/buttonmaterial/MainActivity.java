3ed30439ca8606f207003e931d4ef57bfae22ed9
limudimwork <boriskunda@gmail.com>
2018-02-24 17:08:13 +0200

    Initial commit

---
+++ app/src/main/java/com/happytrees/buttonmaterial/MainActivity.java
@@ -0,0 +1,14 @@
+package com.happytrees.buttonmaterial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //change button background without disabling material effects
+    }
+}
